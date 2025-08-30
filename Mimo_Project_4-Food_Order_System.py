aa816f65953153556fe2c20276f3da2d196ba506
Nicholas Samudio <125914044+nickful123@users.noreply.github.com>
2025-08-30 20:04:28 +0200

    Create Mimo_Project_4-Food_Order_System.py

---
+++ Mimo_Project_4-Food_Order_System.py
@@ -0,0 +1,62 @@
+########################### Variables ###########################
+
+italian_food = [
+  "Pasta Bolognese",
+  "Pepperoni pizza",
+  "Margherita pizza",
+  "Lasagna"
+]
+
+indian_food = [
+  "Curry",
+  "Chutney",
+  "Samosa",
+  "Nan"
+]
+
+########################### Functions ##########################
+
+def find_meal(name, menu):
+  return name if name in menu else None
+
+def select_meal(name, food_type):
+  if food_type == "Italian":
+    return find_meal(name, italian_food)
+  if food_type == "Indian":
+    return find_meal(name, indian_food)
+  else:
+    return None
+
+def display_available_meals(food_type):
+  if food_type == "Italian":
+    print("Available Italian Meals:")
+    for meal in italian_food:
+      print(meal)
+  elif food_type == "Indian":
+    print("Available Indian Meals:")
+    for meal in indian_food:
+      print(meal)
+  else:
+    "Invalid food type"
+
+def create_summary(name, amount, food_type):
+  order = select_meal(name, food_type)
+  if order:
+    return f"You ordered {amount} {food_type} {name} "
+  else:
+    return "Meal not found"
+
+########################### Food Order Script ##################
+
+print("Welcome to the Food Order System!")
+type_input = input("What kind of food would you like? We have Italian and Indian. ")
+print()
+
+display_available_meals(type_input)
+print()
+name_input = input("Enter the name of the meal you want to order: ")
+amount_input = int(input("Enter the quantity you want to order: "))
+print()
+
+result = create_summary(name_input, amount_input, type_input)
+print(result)
