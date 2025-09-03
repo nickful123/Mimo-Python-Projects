56d84fae99f775ae126a045de4d3e4f24f1a62bf
Nicholas Samudio <125914044+nickful123@users.noreply.github.com>
2025-09-03 05:57:07 +0200

    Update Mimo_Project_8-Library.py
    Added Library class 
      1. Adds book variables to list 
      2. Displays book information in console

--- Mimo_Project_8-Library.py
+++ Mimo_Project_8-Library.py
@@ -18,13 +18,39 @@ class Book():
     print(f"Title: {self.title}\n Author: {self.author}")
     print()
 
+###############################################################################
 
+class Library():
+  def __init__(self):
+    self.books = []
+
+  def add_book(self, book):
+    self.books.append(book)
+
+  def display_books(self):
+    for book in self.books:
+      book.display_info()
+
+  def get_book_by_title(self, title):
+    for book in self.books:
+      if title == book.title:
+        return book
+      else:
+        print("Book not in library")
+        return
+
+###############################################################################
 
 book1 = Book("Lord of the Rings", "JRR Tolkien")
 book2 = Book("Game of Thrones", "JR Martin")
 book3 = Book("The Hobbit", "JRR Tolkien")
-books = [book1, book2, book3]
+#books = [book1, book2, book3]
+#for book in books:
+#  book.display_info()
 
 
-for book in books:
-  book.display_info()
+library = Library()
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+library.display_books()
